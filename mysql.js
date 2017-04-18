ab878d1ea0359f6b8d46154b747de00c9a1570b3
João Rafael Santos <joaorafael23@gmail.com>
2017-04-18 23:33:50 +0100

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -1,10 +1,4 @@
 var mysql = require('mysql');
-var express = require('express')
-var app = express();
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-
 var connection = mysql.createConnection({
   host: 'webitcloud.net',
   user: 'webitclo_G501',
@@ -13,25 +7,16 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
+var confs;
 
-// POST method route
-app.post('/home2', function (req, res) {
-  var data = "";
-
-  connection.query('SELECT nome_empresa from Empresa', function (err, rows, fields) {
-    if (!err) {
-      console.log('The solution is: ', rows);
-      console.log("entrei");
-      data = rows;
-      console.log(data);
-      res.send(JSON.stringify(rows));
-    } else {
-      console.log('Error while performing Query.', err);
-    }
-
-  });
-})
-
-
+connection.query('SELECT nome_empresa from Empresa', function (err, rows, fields) {
+  if (!err) {
+    console.log('The solution is: ', rows);
+  }
+  else {
+    console.log('Error while performing Query.', err);
+  }
+  confs = rows;
+});
 
 connection.end();
