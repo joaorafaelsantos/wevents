fd0e41e177a5a62fe6b722b4c9158aba3db1413d
João Rafael Santos <joaorafael23@gmail.com>
2017-04-19 11:05:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/home2', function (req, res) {
+app.get('/home', function (req, res) {
   res.send('Hello World!')
 });
 
