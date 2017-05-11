3dcecc19cca676d76c628b0e21938c8417757569
João Rafael Santos <joaorafael23@gmail.com>
2017-05-11 19:03:48 +0100

    v7
    v7

--- views/assets/common/js/index/index.js
+++ views/assets/common/js/index/index.js
@@ -8,6 +8,8 @@ $(function () {
     $(".nav li").on("click", function () {
         $(".nav li").removeClass("active");
         $(this).addClass("active");
+        $("#navbar-collapse").removeClass("in");
+        $("#btnToggle").addClass("collapsed");
     });
 
     // click event change btnViewMore class (plus/minus)
@@ -37,5 +39,17 @@ $(function () {
         btnViewMore("btnWorkViewMore");
     });
     $('[rel="tooltip"]').tooltip();
+
+
+    // When scroll stops the video
+
+    $(window).scroll(function () {
+        var aboutSectionClass = $("li").first().attr("class");
+        var src = $("#player").attr("src");
+        var url = "https://www.youtube.com/embed/txTqtm58AqM";
+        if (aboutSectionClass != "active" && src == url + "?rel=0&controls=0&autoplay=1;showinfo=0") {
+            $("#player").attr("src", url + "?rel=0&controls=0&autoplay=0;showinfo=0");
+        }
+    });
 
 });
