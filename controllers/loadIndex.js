8849faaa6dab3fc657e82d9ab8fc766feb9c938d
João Rafael Santos <joaorafael23@gmail.com>
2017-05-12 14:10:03 +0100

    v8
    v8

--- controllers/loadIndex.js
+++ controllers/loadIndex.js
@@ -1,12 +1,12 @@
-// var express = require('express');
-// var path = require('path');
-// var app = express();
-// var port = process.env.PORT || 3000;
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = process.env.PORT || 3000;
 
-// app.get('/', function (req, res) {
-//     res.sendFile(path.resolve('views/index.html'));
-// });
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve('views/index.html'));
+});
 
-// app.listen(port, function () {
-//     //console.log('Example app listening on port 3000!')
-// });
+app.listen(port, function () {
+    //console.log('Example app listening on port 3000!')
+});
