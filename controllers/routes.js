fc18c08863c5f67f306dd33e32e7ffbac9fd539a
João Rafael Santos <joaorafael23@gmail.com>
2017-05-22 16:44:41 +0100

    update

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,9 +3,9 @@ var loadEvents = require("../models/loadEvents.js");
 var exports = module.exports = {};
 
 exports.init = function () {
-    global.app.get('/home', function (req, res) {
-        global.request("https://webitcloud.net/PW/1617/JAF/App/views/index.html").pipe(res);
-    });
+    // global.app.get('/home', function (req, res) {
+    //     global.request("https://webitcloud.net/PW/1617/JAF/App/views/index.html").pipe(res);
+    // });
     global.app.post('/home2', function (req, res) {
         console.log("entrei2")
     });
