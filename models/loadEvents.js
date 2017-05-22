dd98fbb62697cea001230c199a199ed810700cce
João Rafael Santos <joaorafael23@gmail.com>
2017-05-22 21:52:42 +0100

    update

--- models/loadEvents.js
+++ models/loadEvents.js
@@ -12,14 +12,12 @@ exports.loadEvents = function (response) {
 
     var events;
     connection.query('SELECT nome_evento, descricao, morada, cidade, pais, data_desc FROM Evento, Localidade, Data_Hora, Categoria WHERE Evento.id_localidade = Localidade.id_localidade AND Evento.id_data_hora = Data_Hora.id_data_hora AND Evento.id_categoria = Categoria.id_categoria;', function (err, rows, fields) {
-        function load(response) {
             if (!err) {
                 events = rows;
                 response.send(events);
             } else {
                 console.log('Error while performing Query.', err);
             }
-        }
     });
 
 };
