b7939116ed0d89aa450ec674f2efd3d789e4994a
João Rafael Santos <joaorafael23@gmail.com>
2017-05-22 18:51:15 +0100

    update

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,13 +4,11 @@ var exports = module.exports = {};
 
 exports.init = function () {
     global.app.get('/home', function (req, res) {
-        // global.request("https://webitcloud.net/PW/1617/JAF/App/views/index.html").pipe(res);
+        global.request("https://webitcloud.net/PW/1617/JAF/App/views/index.html").pipe(res);
         console.log("entrei")
     });
-    global.app.post('/home2', function (req, res) {
+    global.app.post('/loadEvents', function (req, res) {
+        loadEvents.loadEvents();
         console.log("entrei2")
-    });
-    global.app.post('/home3', function (req, res) {
-        console.log("entrei3")
     });
 };
--- models/loadEvents.js
+++ models/loadEvents.js
@@ -1,18 +1,18 @@
 var exports = module.exports = {};
 
 exports.connection = function () {
-    var connection = mysql.createConnection({
+    global.connection = mysql.createConnection({
         host: 'webitcloud.net',
         user: 'webitclo_G501',
         password: 'BD1617G501526',
         database: 'webitclo_G501'
     });
-    connection.connect();
+    global.connection.connect();
 };
 
 exports.loadEvents = function () {
     var events;
-    connection.query('SELECT nome_evento, descricao, morada, cidade, pais, data_desc FROM Evento, Localidade, Data_Hora, Categoria WHERE Evento.id_localidade = Localidade.id_localidade AND Evento.id_data_hora = Data_Hora.id_data_hora AND Evento.id_categoria = Categoria.id_categoria;', function (err, rows, fields) {
+    global.connection.query('SELECT nome_evento, descricao, morada, cidade, pais, data_desc FROM Evento, Localidade, Data_Hora, Categoria WHERE Evento.id_localidade = Localidade.id_localidade AND Evento.id_data_hora = Data_Hora.id_data_hora AND Evento.id_categoria = Categoria.id_categoria;', function (err, rows, fields) {
         if (!err) {
             events = rows;
             res.send(events);
