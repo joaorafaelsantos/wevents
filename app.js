b69369f964977e2060f5a9e0c1ff4b82e3377f56
João Rafael Santos <joaorafael23@gmail.com>
2017-05-22 16:13:33 +0100

    Update
    Update

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var mysql = require('mysql');
-var express = require('express');
+express = require('express');
 var http = require('http');
 var request = require('request');
-var app = express();
+app = express();
 var port = process.env.PORT || 3000;
 
 // files
 var routes = require("./controllers/routes.js");
 
 // load
-
 routes.routes();
 
 
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,17 +1,15 @@
-var module = require('module');
-
-var loadEvents = require("../models/loadEvents.js");
+// var loadEvents = require("../models/loadEvents.js");
 
 var exports = module.exports = {};
 
 exports.routes = function () {
 
-    app.get('/home', function (req, res) {
+    global.app.get('/home', function (req, res) {
         request("https://webitcloud.net/PW/1617/JAF/App/views/index.html").pipe(res);
     });
 
-    app.post('/loadEvents', function (req, res) {
-        loadEvents.loadEvents();
-    });
+    // app.post('/loadEvents', function (req, res) {
+    //     loadEvents.loadEvents();
+    // });
 
 };
