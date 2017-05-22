32d574f08aef88c01144545f49c4811306f47adb
João Rafael Santos <joaorafael23@gmail.com>
2017-05-22 20:37:48 +0100

    update
    update

--- models/loadEvents.js
+++ models/loadEvents.js
@@ -1,23 +1,25 @@
-// var exports = module.exports = {};
+var exports = module.exports = {};
 
-// exports.loadEvents = function () {
+exports.loadEvents = function () {
 
-//     var connection = mysql.createConnection({
-//         host: 'webitcloud.net',
-//         user: 'webitclo_G501',
-//         password: 'BD1617G501526',
-//         database: 'webitclo_G501'
-//     });
-//     connection.connect();
+    var http = require('http');
 
-//     var events;
-//     connection.query('SELECT nome_evento, descricao, morada, cidade, pais, data_desc FROM Evento, Localidade, Data_Hora, Categoria WHERE Evento.id_localidade = Localidade.id_localidade AND Evento.id_data_hora = Data_Hora.id_data_hora AND Evento.id_categoria = Categoria.id_categoria;', function (err, rows, fields) {
-//         if (!err) {
-//             events = rows;
-//             res.send(events);
-//         } else {
-//             console.log('Error while performing Query.', err);
-//         }
-//     });
+    var connection = mysql.createConnection({
+        host: 'webitcloud.net',
+        user: 'webitclo_G501',
+        password: 'BD1617G501526',
+        database: 'webitclo_G501'
+    });
+    connection.connect();
 
-// };
+    var events;
+    connection.query('SELECT nome_evento, descricao, morada, cidade, pais, data_desc FROM Evento, Localidade, Data_Hora, Categoria WHERE Evento.id_localidade = Localidade.id_localidade AND Evento.id_data_hora = Data_Hora.id_data_hora AND Evento.id_categoria = Categoria.id_categoria;', function (err, rows, fields) {
+        if (!err) {
+            events = rows;
+            res.send(events);
+        } else {
+            console.log('Error while performing Query.', err);
+        }
+    });
+
+};
