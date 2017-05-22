bdd1e889dcb37a18df685d6bd89fbcb680149447
João Rafael Santos <joaorafael23@gmail.com>
2017-05-22 16:15:45 +0100

    update
    update

--- controllers/routes.js
+++ controllers/routes.js
@@ -5,6 +5,7 @@ var exports = module.exports = {};
 exports.routes = function () {
 
     global.app.get('/home', function (req, res) {
+        console.log("entrei")
         request("https://webitcloud.net/PW/1617/JAF/App/views/index.html").pipe(res);
     });
 
