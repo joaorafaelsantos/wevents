0644ae2202b1d49279ce02014ea238cfceec1c76
João Rafael Santos <joaorafael23@gmail.com>
2017-05-23 15:24:56 +0100

    Update

--- controllers/routes.js
+++ controllers/routes.js
@@ -8,7 +8,11 @@ exports.init = function () {
         global.request("https://webitcloud.net/PW/1617/JAF/App/views/index.html").pipe(res);
     });
 
-    global.app.post('/loadEvents', function (req, res) {
+    global.app.post('/', function (req, res) {
         loadEvents.loadEvents(res);
+    });
+
+    global.app.get('*', function (req, res) {
+        global.request("https://webitcloud.net/PW/1617/JAF/App/views/pages/errors/404.html").pipe(res);
     });
 };
