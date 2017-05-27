dae490abbd54438ccc752d928bda332613117b27
João Rafael Santos <joaorafael23@gmail.com>
2017-05-27 17:15:48 +0100

    update

--- models/createEvent.js
+++ models/createEvent.js
@@ -20,8 +20,8 @@ exports.createEvent = function (request, response) {
     var address = global.connection.escape(request.body.address);
     var city = global.connection.escape(request.body.city);
     var country = global.connection.escape(request.body.country);
-    var typeEvent = parseInt(global.connection.escape(request.body.selEvent));
-    var privacy = parseInt(global.connection.escape(request.body.optRadio));
+    var typeEvent = global.connection.escape(request.body.selEvent);
+    var privacy = global.connection.escape(request.body.optRadio);
 
     var queryInsert = "INSERT INTO Data_Hora (data_desc, hora) VALUES ('" + date + "', '" + hour + "');";
     var queryInsert2 = "INSERT INTO Localidade (morada, cidade, pais) VALUES (" + address + ", " + city + ", " + country + ");";
