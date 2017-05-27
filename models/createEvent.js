2bb4072287b8be0dca3d53303c21cc51d73c558c
João Rafael Santos <joaorafael23@gmail.com>
2017-05-27 13:06:40 +0100

    update

--- models/createEvent.js
+++ models/createEvent.js
@@ -12,16 +12,17 @@ exports.createEvent = function (request, response) {
     connection.connection();
 
     var name = global.connection.escape(request.body.name);
-    // var date = global.connection.escape(request.body.date);
+    var date = global.connection.escape(request.body.date);
     var date = request.body.date;
-    date = date.split(" ");
-    console.log(date);
-    // var address = global.connection.escape(request.body.address);
-    // var city = global.connection.escape(request.body.city);
-    // var country = global.connection.escape(request.body.country);
-    // var typeEvent = global.connection.escape(request.body.selEvent);
-    // var privacy = global.connection.escape(request.body.optRadio);
-    // console.log(name, date, address, city, country, typeEvent, privacy);
+    var tempDate = date.split(" ");
+    date = date[0];
+    var hour = date[1];
+    var address = global.connection.escape(request.body.address);
+    var city = global.connection.escape(request.body.city);
+    var country = global.connection.escape(request.body.country);
+    var typeEvent = global.connection.escape(request.body.selEvent);
+    var privacy = global.connection.escape(request.body.optRadio);
+    console.log(name, date, address, city, country, typeEvent, privacy);
 
     // connection.connection();
 
