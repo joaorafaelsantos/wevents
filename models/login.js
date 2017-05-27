4c43e2e950d786a5a4f00a86247b5c0744c94015
João Rafael Santos <joaorafael23@gmail.com>
2017-05-27 18:49:36 +0100

    update

--- models/login.js
+++ models/login.js
@@ -6,19 +6,19 @@ var exports = module.exports = {};
 bodyParser.bodyParser();
 
 exports.login = function (request, response) {
-    // connection.connection();
+    connection.connection();
 
     var email = global.connection.escape(request.body.email);
     var password = global.connection.escape(request.body.password);
     console.log(email, password);
 
-    // var query = "SELECT EXISTS(SELECT email, password FROM Utilizador WHERE email = " + email + " AND password = " + password + " +);";
+    var query = "SELECT EXISTS(SELECT email, password FROM Utilizador WHERE email = " + email + " AND password = " + password + ");";
 
-    // global.connection.query(query, function (err, rows, fields) {
-    //     if (!err) {
-    //         console.log(rows)
-    //     } else {
-    //         console.log('Error while performing Query.', err);
-    //     }
-    // });
+    global.connection.query(query, function (err, rows, fields) {
+        if (!err) {
+            console.log(rows)
+        } else {
+            console.log('Error while performing Query.', err);
+        }
+    });
 };
