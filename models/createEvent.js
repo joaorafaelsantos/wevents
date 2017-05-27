1feba571984da98154280928461f026bc553d9a6
João Rafael Santos <joaorafael23@gmail.com>
2017-05-27 17:13:52 +0100

    update

--- models/createEvent.js
+++ models/createEvent.js
@@ -20,8 +20,8 @@ exports.createEvent = function (request, response) {
     var address = global.connection.escape(request.body.address);
     var city = global.connection.escape(request.body.city);
     var country = global.connection.escape(request.body.country);
-    var typeEvent = global.connection.escape(request.body.selEvent);
-    var privacy = global.connection.escape(request.body.optRadio);
+    var typeEvent = parseInt(global.connection.escape(request.body.selEvent));
+    var privacy = parseInt(global.connection.escape(request.body.optRadio));
 
     var queryInsert = "INSERT INTO Data_Hora (data_desc, hora) VALUES ('" + date + "', '" + hour + "');";
     var queryInsert2 = "INSERT INTO Localidade (morada, cidade, pais) VALUES (" + address + ", " + city + ", " + country + ");";
@@ -60,7 +60,6 @@ exports.createEvent = function (request, response) {
     });
 
     global.connection.query("INSERT INTO Evento nome_evento, id_localidade, id_data_hora, id_categoria, privacidade) VALUES (" + name + ", " + tempLocalidade + ", " + tempDataHora + ", " + typeEvent + ", " + privacy + ");", function (err, rows, fields) {
-        console.log(typeof typeEvent)
         console.log("INSERT INTO Evento nome_evento, id_localidade, id_data_hora, id_categoria, privacidade) VALUES (" + name + ", " + tempLocalidade + ", " + tempDataHora + ", " + typeEvent + ", " + privacy + ");")
         if (!err) {
             console.log('Inserted');
