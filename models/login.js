f626751aa63177ab0e7915212dab755c44bba6a1
João Rafael Santos <joaorafael23@gmail.com>
2017-05-28 11:07:59 +0100

    update

--- models/login.js
+++ models/login.js
@@ -10,13 +10,14 @@ exports.login = function (request, response) {
 
     var email = global.connection.escape(request.body.email);
     var password = global.connection.escape(request.body.password);
+    console.log('Email é: ' + email + ', Password é: ' + password);
 
     var query = "SELECT EXISTS(SELECT email, password FROM Utilizador WHERE email = " + email + " AND password = " + password + ") as value;";
 
     global.connection.query(query, function (err, rows, fields) {
         if (!err) {
             if (rows[0].value == 0) {
-                global.request("https://webitcloud.net/PW/1617/JAF/App/views/pages/login/login.html").pipe(response);
+                global.request("https://wevents.herokuapp.com").pipe(response);
             } else {
                 global.request("https://webitcloud.net/PW/1617/JAF/App/views/main.html").pipe(response);
             }
