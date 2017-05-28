da92d6884114040cc265f5b4b930accbfe6c8879
João Rafael Santos <joaorafael23@gmail.com>
2017-05-28 15:33:22 +0100

    update

--- models/login.js
+++ models/login.js
@@ -11,14 +11,15 @@ exports.login = function (request, response, auth) {
     var email = global.connection.escape(request.body.email);
     var password = global.connection.escape(request.body.password);
 
+    console.log(email, password)
+
     var query = "SELECT EXISTS(SELECT email, password FROM Utilizador WHERE email = " + email + " AND password = " + password + ") as value;";
     global.auth = false;
     global.connection.query(query, function (err, rows, fields) {
         if (!err) {
-            // if (rows[0].value != 0) {
-            //     global.auth = true;
-            // }
-            console.log("tamanho: " + rows[0].value);
+            if (rows[0].value != 0) {
+                global.auth = true;
+            }
         } else {
             console.log('Error while performing Query.', err);
             global.request("https://wevents.herokuapp.com").pipe(response);
