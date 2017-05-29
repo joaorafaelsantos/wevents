25042d076710a6bc53adc1b1b71c1bc7b6243102
João Rafael Santos <joaorafael23@gmail.com>
2017-05-29 19:55:24 +0100

    update

--- controllers/routes.js
+++ controllers/routes.js
@@ -9,7 +9,7 @@ exports.init = function () {
 
     global.app.use(global.cookieSession({
         name: 'session',
-        keys: [],
+        keys: ['key1', 'key2'],
         // Cookie Options
         maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }))
