80c283b8f1f745efaae0a41656c27d456e261db1
João Rafael Santos <joaorafael23@gmail.com>
2017-05-29 08:54:50 +0100

    update

--- models/connection.js
+++ models/connection.js
@@ -2,11 +2,11 @@ var exports = module.exports = {};
 
 exports.connection = function () {
 
-    // global.connection = mysql.createConnection({
-    //     host: 'webitcloud.net',
-    //     user: 'webitclo_G501',
-    //     password: 'BD1617G501526',
-    //     database: 'webitclo_G501'
-    // });
-    // global.connection.connect();
+    global.connection = mysql.createConnection({
+        host: 'webitcloud.net',
+        user: 'webitclo_G501',
+        password: 'BD1617G501526',
+        database: 'webitclo_G501'
+    });
+    global.connection.connect();
 };
