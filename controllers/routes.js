2c0d87c58571f7d6220e3357693cd3d0c8d58888
João Rafael Santos <joaorafael23@gmail.com>
2017-05-30 11:01:19 +0100

    update

--- controllers/routes.js
+++ controllers/routes.js
@@ -10,7 +10,7 @@ exports.init = function () {
     app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://wevents.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
