0bb99d99ef63a715c821ec7e1fe17f3ca100b027
João Rafael Santos <joaorafael23@gmail.com>
2017-05-30 11:40:26 +0100

    update

--- models/cookieSession.js
+++ models/cookieSession.js
@@ -5,6 +5,6 @@ exports.cookieSession = function () {
         name: 'session',
         keys: ['key1', 'key2'],
         // Cookie Options
-        maxAge: 0.5 * 60 * 60 * 10000 // 30 minutes
+        maxAge: 1 * 60 * 60 * 1000 // 1 hour
     }));
 }
