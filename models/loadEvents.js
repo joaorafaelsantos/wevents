f8bfc4fe0de68e98f99263f7e04a6af0afa43577
João Rafael Santos <joaorafael23@gmail.com>
2017-05-30 18:37:40 +0100

    update

--- models/loadEvents.js
+++ models/loadEvents.js
@@ -5,7 +5,7 @@ var exports = module.exports = {};
 exports.loadEvents = function (response) {
     connection.connection();
     var events;
-    var query = 'SELECT nome_evento, descricao, morada, cidade, pais, data_desc FROM Evento, Localidade, Data_Hora, Categoria WHERE Evento.id_localidade = Localidade.id_localidade AND Evento.id_data_hora = Data_Hora.id_data_hora AND Evento.id_categoria = Categoria.id_categoria AND Evento.privacidade = 0;';
+    var query = 'SELECT nome_evento as name, descricao as description, morada as address, cidade as city, pais as country, data_desc as date FROM Evento, Localidade, Data_Hora, Categoria WHERE Evento.id_localidade = Localidade.id_localidade AND Evento.id_data_hora = Data_Hora.id_data_hora AND Evento.id_categoria = Categoria.id_categoria AND Evento.privacidade = 0;';
     global.connection.query(query, function (err, rows, fields) {
         if (!err) {
             events = rows;
