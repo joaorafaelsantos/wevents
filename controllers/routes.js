6782f01f4122bf2944153b02923f306cf03e67e4
João Rafael Santos <joaorafael23@gmail.com>
2017-06-04 21:32:44 +0100

    v11 - fix2
    v11 - fix2

--- controllers/routes.js
+++ controllers/routes.js
@@ -11,54 +11,54 @@ exports.init = function () {
         global.request("https://webitcloud.net/PW/1617/JAF/App/views/index.html").pipe(res);
     });
 
-    // // Load portal
-    // global.app.get('/portal', function (req, res) {
-    //     access.redirectPortal(req, res);
-    // });
+    // Load portal
+    global.app.get('/portal', function (req, res) {
+        access.redirectPortal(req, res);
+    });
 
-    // /* Login */
+    /* Login */
 
-    // // Check login
-    // global.app.post('/login/checkLogin', function (req, res) {
-    //     login.checkLogin(req, res);
-    // });
+    // Check login
+    global.app.post('/login/checkLogin', function (req, res) {
+        login.checkLogin(req, res);
+    });
 
-    // // Get user
-    // global.app.post('/login/getUser', function (req, res) {
-    //     login.getUser(req, res);
-    // });
+    // Get user
+    global.app.post('/login/getUser', function (req, res) {
+        login.getUser(req, res);
+    });
 
-    // // Facebook
+    // Facebook
 
-    // global.app.post('/login/facebook', function (req, res) {
-    //     login.checkLoginFacebook(req, res);
-    // });
+    global.app.post('/login/facebook', function (req, res) {
+        login.checkLoginFacebook(req, res);
+    });
 
-    // // Logout
+    // Logout
 
-    // global.app.post('/login/logout', function (req, res) {
-    //     login.logout(req, res);
-    // });
+    global.app.post('/login/logout', function (req, res) {
+        login.logout(req, res);
+    });
 
-    // // Create user ***
-    // global.app.post('/login/createUser', function (req, res) {
-    //     login.createUser();
-    // });
+    // Create user ***
+    global.app.post('/login/createUser', function (req, res) {
+        login.createUser();
+    });
 
-    // /* Events */
+    /* Events */
 
-    // // Get Events (only public events)
-    // global.app.post('/events/getEvents', function (req, res) {
-    //     events.loadEvents(res);
-    // });
+    // Get Events (only public events)
+    global.app.post('/events/getEvents', function (req, res) {
+        events.loadEvents(res);
+    });
 
-    // // Create event
-    // global.app.post('/events/createEvent', function (req, res) {
-    //     events.createEvent(req, res);
-    // });
+    // Create event
+    global.app.post('/events/createEvent', function (req, res) {
+        events.createEvent(req, res);
+    });
 
-    // // 404 error handler
-    // global.app.get('*', function (req, res) {
-    //     global.request("https://webitcloud.net/PW/1617/JAF/App/views/pages/errors/404.html").pipe(res);
-    // });
+    // 404 error handler
+    global.app.get('*', function (req, res) {
+        global.request("https://webitcloud.net/PW/1617/JAF/App/views/pages/errors/404.html").pipe(res);
+    });
 };
