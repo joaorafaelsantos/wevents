f2702423b4b856af271d0600dff890fb5cd8e32d
João Rafael Santos <joaorafael23@gmail.com>
2017-06-05 14:20:13 +0100

    update

--- models/login/login.js
+++ models/login/login.js
@@ -15,7 +15,7 @@ exports.createUser = function (request, response) {
     var email = global.connection.escape(request.body.email);
     var password = global.connection.escape(request.body.password);
 
-    var query = "INSERT INTO Utilizador (password, email, id_tipo_utilizador) VALUES (" + password + "," + email + ", 2');";
+    var query = "INSERT INTO Utilizador (password, email, id_tipo_utilizador) VALUES (" + password + "," + email + ", 2);";
 
     global.connection.query(query, function (err, rows, fields) {
         if (!err) {
