942cd6c3af935a04720b77fa3ffb096f84dcdaa3
João Rafael Santos <joaorafael23@gmail.com>
2017-06-06 15:30:47 +0100

    update

--- controllers/routes.js
+++ controllers/routes.js
@@ -61,6 +61,7 @@ exports.init = function () {
     // Get User Events
     global.app.post('/events/getUserEvents', function (req, res) {
         events.loadUserEvents(req, res);
+        console.log(req.session.id);
     });
 
     // Create event
