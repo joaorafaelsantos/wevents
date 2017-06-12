823a63515b1bbadb0477e9b345916308dd368d49
João Rafael Santos <joaorafael23@gmail.com>
2017-06-12 12:22:48 +0100

    update

--- models/events/events.js
+++ models/events/events.js
@@ -73,15 +73,14 @@ exports.createEvent = function (request, response) {
             console.log('Error while performing Query.', err);
         }
     });
-    // global.connection.query("INSERT INTO Evento (nome_evento, id_localidade, id_data_hora, id_categoria, privacidade, img_url) VALUES (" + name + ", " + tempLocalidade + ", " + tempDataHora + ", " + typeEvent + ", " + privacy + ", " + url + ");", function (err, rows, fields) {
-    //     console.log("INSERT INTO Evento (nome_evento, id_localidade, id_data_hora, id_categoria, privacidade) VALUES (" + name + ", " + tempLocalidade + ", " + tempDataHora + ", " + typeEvent + ", " + privacy + ");")
-    //     if (!err) {
-    //         console.log('Inserted');
-    //     } else {
-    //         console.log('Error while performing Query.', err);
-    //     }
-    // });
-    console.log("DADOS: ", tempDataHora, tempLocalidade);
+    global.connection.query("INSERT INTO Evento (nome_evento, id_localidade, id_data_hora, id_categoria, privacidade, img_url) VALUES (" + name + ", " + tempLocalidade + ", " + tempDataHora + ", " + typeEvent + ", " + privacy + ", " + url + ");", function (err, rows, fields) {
+        console.log(tempDataHora, tempLocalidade)
+        if (!err) {
+            console.log('Inserted');
+        } else {
+            console.log('Error while performing Query.', err);
+        }
+    });
 };
 
 // Load events
