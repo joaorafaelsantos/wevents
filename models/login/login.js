da8da1b46bf2b51d8dce3c6dce329fd476b543ab
João Rafael Santos <joaorafael23@gmail.com>
2017-06-14 11:58:43 +0100

    update

--- models/login/login.js
+++ models/login/login.js
@@ -9,28 +9,28 @@ cookieSession.cookieSession();
 
 // Create user
 
-exports.createUser = function (request, response) {
-    connection.connection();
+// exports.createUser = function (request, response) {
+//     connection.connection();
 
-    var email = global.connection.escape(request.body.email);
-    var password = global.connection.escape(request.body.password);
-    var name = global.connection.escape(request.body.name);
+//     var email = global.connection.escape(request.body.email);
+//     var password = global.connection.escape(request.body.password);
+//     var name = global.connection.escape(request.body.name);
 
 
-    var query = "INSERT INTO Utilizador (password, email, id_tipo_utilizador, nome) VALUES (" + password + "," + email + ", 2," + name + ");"
+//     var query = "INSERT INTO Utilizador (password, email, id_tipo_utilizador, nome) VALUES (" + password + "," + email + ", 2," + name + ");"
 
-    global.connection.query(query, function (err, rows, fields) {
-        if (!err) {
-            console.log('Inserted');
-            response.send("success");
-            // global.request("https://wevents.herokuapp.com").pipe(response);
-        } else {
-            console.log('Error while performing Query.', err);
-            //global.request("https://wevents.herokuapp.com").pipe(response);
-            response.send("fail");
-        }
-    });
-};
+//     global.connection.query(query, function (err, rows, fields) {
+//         if (!err) {
+//             console.log('Inserted');
+//             response.send("success");
+//             // global.request("https://wevents.herokuapp.com").pipe(response);
+//         } else {
+//             console.log('Error while performing Query.', err);
+//             //global.request("https://wevents.herokuapp.com").pipe(response);
+//             response.send("fail");
+//         }
+//     });
+// };
 
 // Check normal login
 
