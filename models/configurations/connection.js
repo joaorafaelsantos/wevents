8796fa54a1217cba78ef28ff92b2e98b4a574400
João Rafael Santos <joaorafael23@gmail.com>
2017-06-15 13:07:38 +0100

    update

--- models/configurations/connection.js
+++ models/configurations/connection.js
@@ -4,9 +4,11 @@ exports.connection = function () {
 
     global.connection = mysql.createConnection({
         host: 'webitcloud.net',
-        user: 'webitclo_jaf4',
-        password: 'sNRJy8t@yLzK',
+        user: 'webitclo_G501',
+        password: 'PW2G501567',
         database: 'webitclo_G501'
+
+        // sNRJy8t@yLzK
     });
     global.connection.connect();
 };
