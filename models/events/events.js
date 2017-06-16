1898fe32b8872854837fb8d6b803b9b74ee3d062
João Rafael Santos <joaorafael23@gmail.com>
2017-06-16 03:03:12 +0100

    update

--- models/events/events.js
+++ models/events/events.js
@@ -112,12 +112,13 @@ exports.subscribeEvent = function (request, response) {
     var events;
     var id = request.session.id;
     var id_event = request.body.id;
-    var query = "INSERT INTO Registo(id_evento, id_utilizador) SELECT DISTINCT" + id_event + ", " + id + "FROM Registo WHERE (SELECT (SELECT COUNT(*) FROM Registo WHERE id_evento =" + id_event + ") < (SELECT capacidade FROM Evento WHERE id_evento =" + id_event + ")) = 1;";
-    global.connection.query(query, function (err, rows, fields) {
-        if (!err) {
-            console.log(rows.length);
-        } else {
-            console.log('Error while performing Query.', err);
-        }
-    });
+    var query = "INSERT INTO Registo(id_evento, id_utilizador) SELECT DISTINCT " + id_event + ", " + id + "FROM Registo WHERE (SELECT (SELECT COUNT(*) FROM Registo WHERE id_evento = " + id_event + ") < (SELECT capacidade FROM Evento WHERE id_evento =" + id_event + ")) = 1;";
+    console.log(query)
+    // global.connection.query(query, function (err, rows, fields) {
+    //     if (!err) {
+    //         console.log(rows.length);
+    //     } else {
+    //         console.log('Error while performing Query.', err);
+    //     }
+    // });
 };
