26e3a4b1273fc1e7a245691e0507923671b22a11
João Rafael Santos <joaorafael23@gmail.com>
2017-06-16 19:31:29 +0100

    update

--- models/configurations/nodemailer.js
+++ models/configurations/nodemailer.js
@@ -36,7 +36,7 @@ exports.sendEmail = function (request, response) {
         from: 'jaf@webitcloud.net',
         to: recEmail,
         subject: 'worldevents - New Password',
-        html: "</center><b>worldevents</b><br><img src='https://webitcloud.net/PW/1617/JAF/App/views/assets/common/img/logo/logo144.png</img><br>" + text + "<br>Your worldevents team!</center>'>"
+        html: "<center><b>worldevents</b><br><img src='https://webitcloud.net/PW/1617/JAF/App/views/assets/common/img/logo/logo144.png'</img><br>" + text + "<br>Your worldevents team!</center>"
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
