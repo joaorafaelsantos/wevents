057879f24b2cf36a47aa6cb34a0d00d1f158f5b4
João Rafael Santos <joaorafael23@gmail.com>
2017-06-17 17:36:16 +0100

    update

--- models/access/access.js
+++ models/access/access.js
@@ -8,8 +8,8 @@ cookieSession.cookieSession();
 
 exports.redirectPortal = function (request, response) {
     if (request.session.email != undefined && request.session.password != undefined) {
-        console.log("entrei1")
         var key = "*\~/*" + request.session.email + "*\./*" + request.session.password + "*\|/*" + request.session.password.length + "*\%/*" + request.session.email.length + "*\}/*" + "tsiw_2017" + "*\ª/*";
+        console.log(key);
         if (request.session.key == key) {
             global.request("https://webitcloud.net/PW/1617/JAF/App/views/main.html").pipe(response);
         } else {
@@ -17,7 +17,6 @@ exports.redirectPortal = function (request, response) {
         }
 
     } else {
-        console.log("entrei2")
         global.request("https://webitcloud.net/PW/1617/JAF/App/views/pages/errors/403.html").pipe(response);
     }
 }
