0a5410a86cc179760c8ec2307e5bceb9d398d7ca
João Rafael Santos <joaorafael23@gmail.com>
2017-06-17 15:05:16 +0100

    update

--- models/access/access.js
+++ models/access/access.js
@@ -7,8 +7,8 @@ cookieSession.cookieSession();
 // Redirect to portal page
 
 exports.redirectPortal = function (request, response) {
-    if (request.session.user != undefined && request.session.password != undefined) {
-        var key = "*\~/*" + request.session.user + "*\./*" + request.session.password + "*\|/*" + request.session.password.length + "*\%/*" + request.session.user.length + "*\}/*" + "tsiw_2017" + "*\ª/*";
+    if (request.session.name != undefined && request.session.password != undefined) {
+        var key = "*\~/*" + request.session.name + "*\./*" + request.session.password + "*\|/*" + request.session.password.length + "*\%/*" + request.session.user.length + "*\}/*" + "tsiw_2017" + "*\ª/*";
         if (request.session.key == key) {
             global.request("https://webitcloud.net/PW/1617/JAF/App/views/main.html").pipe(response);
         } else {
