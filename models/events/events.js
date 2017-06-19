5a12741f5759e278420599481f2b6fdb28761f73
João Rafael Santos <joaorafael23@gmail.com>
2017-06-19 12:27:12 +0100

    update

--- models/events/events.js
+++ models/events/events.js
@@ -165,3 +165,33 @@ exports.subscribeEvent = function (request, response) {
         }
     });
 };
+
+
+// exports.subscribeEvent = function (request, response) {
+//     connection.connection();
+//     var events;
+//     var id = request.session.id;
+//     var id_event = request.body.id;
+//     var key = request.body.key;
+//     var query = '';
+
+//     if (key != undefined) {
+//         id_event = "(SELECT id_evento FROM Evento WHERE chave = '" + key + "' )";
+//         query = "INSERT INTO Registo(id_evento, id_utilizador) SELECT DISTINCT " + id_event + ", " + id + " FROM Registo WHERE (SELECT (SELECT COUNT(*) FROM Registo WHERE id_evento = " + id_event + ") < (SELECT capacidade FROM Evento WHERE id_evento = " + id_event + ") = 1) AND " + id_event + " NOT IN (SELECT id_evento FROM Evento WHERE id_utilizador_criador = " + id + ");";
+//     } else {
+//         query = "INSERT INTO Registo(id_evento, id_utilizador) SELECT DISTINCT " + id_event + ", " + id + " FROM Registo WHERE (SELECT (SELECT COUNT(*) FROM Registo WHERE id_evento = " + id_event + ") < (SELECT capacidade FROM Evento WHERE id_evento = " + id_event + ") = 1) AND " + id_event + " NOT IN (SELECT id_evento FROM Evento WHERE id_utilizador_criador = " + id + ");";
+//     }
+
+//     global.connection.query(query, function (err, result) {
+//         if (!err) {
+//             var numRows = result.affectedRows;
+//             if (numRows == 1) {
+//                 response.send("success");
+//             } else {
+//                 response.send("fail");
+//             }
+//         } else {
+//             response.send("fail");
+//         }
+//     });
+// };
