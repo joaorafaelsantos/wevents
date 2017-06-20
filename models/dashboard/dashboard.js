a386ae869246391ed590eca0891d99142d873d42
João Rafael Santos <joaorafael23@gmail.com>
2017-06-20 19:20:54 +0100

    update

--- models/dashboard/dashboard.js
+++ models/dashboard/dashboard.js
@@ -7,7 +7,7 @@ var exports = module.exports = {};
 exports.loadEvents = function (response) {
     connection.connection();
     var events;
-    var query = 'SELECT privacidade as privacy, id_utilizador_criador as user_creator, chave as key, img_url as image, nome_evento as name, descricao as description, morada as address, cidade as city, pais as country, data_desc as date FROM Evento, Localidade, Data_Hora, Categoria WHERE Evento.id_localidade = Localidade.id_localidade AND Evento.id_data_hora = Data_Hora.id_data_hora AND Evento.id_categoria = Categoria.id_categoria;';
+    var query = 'SELECT privacidade as privacy, id_utilizador_criador as user_creator, chave as secretKey, img_url as image, nome_evento as name, descricao as description, morada as address, cidade as city, pais as country, data_desc as date FROM Evento, Localidade, Data_Hora, Categoria WHERE Evento.id_localidade = Localidade.id_localidade AND Evento.id_data_hora = Data_Hora.id_data_hora AND Evento.id_categoria = Categoria.id_categoria;';
     global.connection.query(query, function (err, rows, fields) {
         if (!err) {
             events = rows;
