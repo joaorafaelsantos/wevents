aa66300ef4be5ba4b0ba7db44aef0c34aaacff44
João Rafael Santos <joaorafael23@gmail.com>
2017-06-20 10:30:46 +0100

    update

--- models/configurations/connection.js
+++ models/configurations/connection.js
@@ -4,7 +4,7 @@ exports.connection = function () {
 
     global.connection = mysql.createConnection({
         host: 'webitcloud.net',
-        user: 'webitclo_jaf4',
+        user: 'webitclo_jaf5',
         password: 'sNRJy8t@yLzK',
         database: 'webitclo_G501'
 
